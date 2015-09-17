c1bf8ee29c460829379e89c7f54a941abf3b4985
Jürgen Pfadt <Juergen.Pfadt@ipa.fraunhofer.de>
2015-09-17 14:26:47 +0000

    batch script added for several areas in ipa office

---
+++ cob_default_env_config/ipa-office/create_local_grid_maps_batch.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+###
+# inner stairwell
+./create_local_grid_maps_manual.sh 757 530 165 219
+
+###
+# outer stairwell- north
+./create_local_grid_maps_manual.sh 651 417 321 78
+
+# outer stairwell- east
+./create_local_grid_maps_manual.sh 964 493 57 298
+
+# outer stairwell- south
+./create_local_grid_maps_manual.sh 645 777 330 83
+
+# outer stairwell- west
+./create_local_grid_maps_manual.sh 603 468 71 325
+
+###
+# floor northwest
+./create_local_grid_maps_manual.sh 364 190 333 326
+
+
+###
+# floor south west - entrance
+./create_local_grid_maps_manual.sh 578 706 153 159
+
+# floor south west - 1st door right
+./create_local_grid_maps_manual.sh 453 766 112 114
+
+# floor south west - 2nd door right
+./create_local_grid_maps_manual.sh 394 878 108 74
+
+# floor south west - 3rd door right
+./create_local_grid_maps_manual.sh 146 946 214 142
+
+# floor south west - 1st door left
+./create_local_grid_maps_manual.sh 551 890 151 312
+
+# floor south west - ending
+./create_local_grid_maps_manual.sh 268 1076 146 93
+
+
+###
+# floor south east
+./create_local_grid_maps_manual.sh 923 756 171 150
+
+
+###
+# floor north east - entrance
+./create_local_grid_maps_manual.sh 903 391 157 158
+
+# floor north east - 1st door right
+./create_local_grid_maps_manual.sh 1050 393 160 162
+
+# floor north east - 2nd door right
+./create_local_grid_maps_manual.sh 1251 186 249 192
+
+# floor north east - 1st door left
+./create_local_grid_maps_manual.sh 886 280 171 114
+
+# floor north east - 2nd door left
+./create_local_grid_maps_manual.sh 1043 209 91 112
+
+# floor north east - 3rd door left
+./create_local_grid_maps_manual.sh 1131 0 141 187
+
+# floor north east - ending
+./create_local_grid_maps_manual.sh 1284 19 181 159
+
+
+
+
+
+
+
+
+
+
+
+
+
